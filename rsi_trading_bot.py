5fd18dbd94ad750842f0ff5469b7214287047d24
Vladislav Prokopev <vlad.prokopev.0660@mail.ru>
2026-03-26 14:29:16 +0800

    Update rsi_trading_bot.py

--- rsi_trading_bot.py
+++ rsi_trading_bot.py
@@ -12,7 +12,7 @@ QTY_DECIMALS = 0
 POLL_INTERVAL = 60
 RSI_PERIOD = 7
 RSI_THRESHOLD = 50
-TAKE_PROFIT_PCT = 0.022      # +2%
+TAKE_PROFIT_PCT = 0.02      # +2%
 STOP_LOSS_PCT = 0.03       # -3%
 
 logging.basicConfig(
